34fbd38d37c73b907e8b05a7bfab283683fb3a2a
Marcos Mota <marcosmota5@hotmail.com>
2024-10-14 00:58:24 -0400

    Added initial project files, including models for contact and category

---
+++ Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactManager.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Please enter the category name")]
+    public string Name { get; set; }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContactManager.Models;
+
+public class Contact
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Please enter the first name")]
+    public string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Please enter the last name")]
+    public string LastName { get; set; }
+
+    [Required(ErrorMessage = "Please enter the phone number")]
+    [Length(12, 12, ErrorMessage = "The phone number must be in the format 000-000-0000")]
+    public string Phone { get; set; }
+
+    [Required(ErrorMessage = "Please enter the email")]
+    [EmailAddress(ErrorMessage = "The typed value is not a valid email address")]
+    public string Email { get; set; }
+
+    [ValidateNever]
+    public DateTime DateAdded { get; set; }
+
+    [Required(ErrorMessage = "Please select a category.")]
+    public int CategoryId { get; set; }
+
+    [ValidateNever]
+    public Category? Category { get; set; }
+
+    public Contact()
+    {
+        this.DateAdded = DateTime.UtcNow;
+    }
+}
